580b6ac6947cb2b488868fdb2df3fe607fba3d79
LMY <lmun768@naver.com>
2020-08-01 10:00:58 +0900

    [Add] AddSongActivity

---
+++ app/src/main/java/com/junhyuk/narshamusicproject/AddSong.java
@@ -0,0 +1,14 @@
+package com.junhyuk.narshamusicproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_song);
+    }
+}
